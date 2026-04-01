d87b147ba0d52d4ca490238540d46012e7045073
leotuor <leotuor@hotmail.com>
2026-03-31 22:01:00 -0300

    feat: adicionado animacao especial

--- player/playerMovementBasic.gd
+++ player/playerMovementBasic.gd
@@ -5,7 +5,10 @@ const JUMP_VELOCITY = -400.0
 
 @onready var AS = $AnimatedSprite2D
 @onready var steak = $"../SteakPickup"
-var is_attacking = false # This is our "lock"
+
+# We use these to "lock" the player's state
+var is_attacking = false 
+var is_using_special = false 
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -14,14 +17,18 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# 1. Trigger the attack here
-	if Input.is_action_just_pressed("attack") and is_on_floor():
+	# 1. Trigger the standard attack
+	if Input.is_action_just_pressed("attack") and is_on_floor() and not is_using_special:
 		attack()
+
+	# 2. Trigger the special ability (Requires a new input action mapped in Project Settings)
+	if Input.is_action_just_pressed("special_action") and is_on_floor() and not is_attacking:
+		special_ability()
 
 	var direction := Input.get_axis("move_left", "move_right")
 	
-	# Only allow turning/moving if not attacking
-	if not is_attacking:
+	# 3. Only allow turning/moving if NOT attacking and NOT using the special ability
+	if not is_attacking and not is_using_special:
 		if direction > 0:
 			AS.flip_h = false
 		elif direction < 0:
@@ -32,25 +39,26 @@ func _physics_process(delta: float) -> void:
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 	else:
-		# Optional: stop movement while attacking
+		# Stop movement while attacking or using special ability
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 	handle_animation()
-	take_damage(10)
+	special_ability()
 
 func attack():
 	is_attacking = true
 	AS.play("attack")
 
-func take_damage(damage):
-	if Input.is_action_just_pressed("pick_item_btn"):
-		GameManager.update_hp(-damage)
-
-
+# 4. The new special ability function
+func special_ability():
+	if Input.is_action_just_pressed("special_action"):
+		is_using_special = true
+		AS.play("special_ability")
 
 func handle_animation():
-	if is_attacking:
+	# 5. Skip standard animations if EITHER lock is active
+	if is_attacking or is_using_special:
 		return 
 
 	if is_on_floor():
@@ -61,6 +69,9 @@ func handle_animation():
 	else:
 		AS.play("running")
 
+# 6. Unlock the respective state when its animation finishes
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if AS.animation == "attack":
 		is_attacking = false
+	elif AS.animation == "special_ability":
+		is_using_special = false
