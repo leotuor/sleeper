8e148070d7e4b5e7349e01182a6bd7fcf6af4b92
Gabriel Riva <gabriielriiva@gmail.com>
2026-04-08 20:44:23 -0300

    PauseMenu

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -3,6 +3,24 @@ extends Control
 func _on_play_pressed() -> void:
 	GameManager.start_game()
 
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_settings_pressed() -> void:
+	$Menu.visible = false
+	$Settings.visible = true
+
+func _on_back_pressed() -> void:
+	$Menu.visible = true
+	$Settings.visible = false
+	$Video.visible = false
+	$Audio.visible = false
+	$Controls.visible = false
+
+
+func _on_video_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_audio_pressed() -> void:
+	pass # Replace with function body.
---
+++ pausemenu.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+@onready var resume: Button = $menuholder/resume
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		visible = true
+		get_tree().paused = true
+		resume.grab_focus()
+
+
+func _on_resume_pressed() -> void:
+	get_tree().paused = false
+	visible = false
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
